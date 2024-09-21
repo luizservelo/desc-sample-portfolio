da60eb379b32dc8a4f90f27224b5c9b52b7a3cae
Luiz Servelo <luiz@torors.com.br>
2024-09-20 23:39:50 -0300

    feat: add website to github

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// Função simples de envio de formulário (exemplo)
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+  alert('Mensagem enviada com sucesso!');
+});
